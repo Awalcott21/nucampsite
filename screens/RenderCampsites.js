3532f317dcd2140912aa612f0a6888dd53c65c4d
Alex Walcott <awalcott213@gmail.com>
2023-04-12 18:20:23 -0400

    Social Sharing

--- screens/RenderCampsites.js
+++ screens/RenderCampsites.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
-import { panResponder, Alert } from 'react-native';
+import { panResponder, Alert, Share } from 'react-native';
 import { useRef } from 'react';
 
 const RenderCampsite = (props) => {
@@ -47,6 +47,21 @@ const RenderCampsite = (props) => {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title,
+                message: `${title}: ${message} ${url}`,
+                url
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        );
+    };
+
+
+
     if (campsite) {
         return (
             <Animatable.View
@@ -92,39 +107,54 @@ const RenderCampsite = (props) => {
                             reverse
                             onPress={() => onShowModal()}
                         />
+                        <Icon
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
+                        />
                     </View >
                 </Card>
             </Animatable.View>
         );
-    }};
-    return <View />;
+    }
+};
+return <View />;
 
 
 
-    const styles = StyleSheet.create({
-        cardContainer: {
-            padding: 0,
-            margin: 0,
-            marginBottom: 20
-        },
+const styles = StyleSheet.create({
+    cardContainer: {
+        padding: 0,
+        margin: 0,
+        marginBottom: 20
+    },
 
-        cardRow: {
-            alignments: 'center',
-            justifyContent: 'center',
-            flex: 1,
-            flexDirection: 'row',
-            margin: '20'
-        },
+    cardRow: {
+        alignments: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: '20'
+    },
 
-        cardText: {
-            textShadowColor: 'rgba(0,0,0, 1)',
-            textShadowOffset: { width: -1, height: 1 },
-            textShadowRadius: '20',
-            textAlign: 'center',
-            color: 'white',
-            fontSize: '20'
-        }
+    cardText: {
+        textShadowColor: 'rgba(0,0,0, 1)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: '20',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: '20'
+    }
 
-    });
+});
 
-    export default RenderCampsite;
+export default RenderCampsite;
